d8af05fa90a57763fd4adc43383a79342d4358d9
Chazona <chznbaum@gmail.com>
2017-04-13 02:16:21 +0000

    integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,18 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+  def new
+    @portfolio_item = Portfolio.new
+  end
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item has been created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
