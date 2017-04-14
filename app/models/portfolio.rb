2215f00e0c341be2836c5472bfc0e68ce082db2c
Chazona <chznbaum@gmail.com>
2017-04-14 20:30:55 +0000

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,4 +5,10 @@ class Portfolio < ApplicationRecord
 
   scope :vue, -> { where(subtitle: 'Vue') }
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
 end
