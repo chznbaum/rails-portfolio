a16de49d8a63354f7aca9408c9bd3aa840cd153b
Chazona <chznbaum@gmail.com>
2017-04-15 14:13:41 +0000

    refactored application controller to include concern for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  include SetSource
 
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
 end
