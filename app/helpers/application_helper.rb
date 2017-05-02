1b13d784ceeb8f2080d18c05513fd949b6acfbc0
Chazona <chznbaum@gmail.com>
2017-05-02 16:31:35 -0400

    fixed bug with login helper method by setting defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def login_helper(style)
+    def login_helper style = ''
         if current_user.is_a?(GuestUser)
             (link_to "Register", new_user_registration_path, class: style) + " " +
             (link_to "Login", new_user_session_path, class: style)
@@ -8,7 +8,7 @@ module ApplicationHelper
         end
     end
 
-    def source_helper(layout_name)
+    def source_helper layout_name
         if session[:source]
             greeting = "Thanks for visiting from #{session[:source]}!"
             content_tag(:p, greeting, class: "source-#{layout_name}")
