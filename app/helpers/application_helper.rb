a5d873da1872294dbf00c60cf31d75f965079766
Chazona <chznbaum@gmail.com>
2017-05-03 01:27:45 -0400

    implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,42 @@ module ApplicationHelper
     def copyright_generator
         @copyright = ChznbaumViewTool::Renderer.copyright 'Chazona Baum', 'All rights reserved'
     end
+
+    def nav_items
+        [
+            {
+                url: root_path,
+                title: 'Home'
+            },
+            {
+                url: about_path,
+                title: 'About'
+            },
+            {
+                url: portfolios_path,
+                title: 'Portfolio'
+            },
+            {
+                url: contact_path,
+                title: 'Contact'
+            },
+            {
+                url: blogs_path,
+                title: 'Blog'
+            },
+        ]
+    end
+
+    def nav_helper style, tag_type
+        nav_links = ''
+        nav_items.each do |item|
+            nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+        end
+
+        nav_links.html_safe
+    end
+
+    def active? path
+        "active" if current_page? path
+    end
 end
