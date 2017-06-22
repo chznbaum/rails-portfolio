7fe33f03257c619d7e09538870b1dd4fc1b5b917
Chazona <chznbaum@gmail.com>
2017-06-21 22:26:40 -0400

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,6 +4,9 @@ class Portfolio < ApplicationRecord
 
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  mount_uploader :main_image, PortfolioUploader
+  mount_uploader :thumb_image, PortfolioUploader
 
   def self.by_position
     order("position ASC")
