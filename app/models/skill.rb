873cc380cba7016bc225f5fb73b33cbb76407ed5
Chazona <chznbaum@gmail.com>
2017-06-21 23:45:53 -0400

    Refactored placeholder functionality into view

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,3 @@
 class Skill < ApplicationRecord
-  include Placeholder
   validates_presence_of :title, :percent_utilized
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(width: '250', height: '250')
-  end
 end
