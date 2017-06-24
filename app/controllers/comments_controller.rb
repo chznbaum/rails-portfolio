d0f0b4ce6dee5e402b2342e40ea174d7aac34708
Chazona <chznbaum@gmail.com>
2017-06-24 15:10:52 -0400

    Built out initial form and partial call for comments along with controller actions.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.build(comment_params)
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
