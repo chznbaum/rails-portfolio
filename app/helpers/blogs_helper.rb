190b288b16c4e32c8766733f2a7e3bfc10cc3d8c
Chazona <chznbaum@gmail.com>
2017-06-25 14:00:29 -0400

    Implemented partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,8 @@ module BlogsHelper
   def gravatar_helper user
     image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", class: 'img-fluid'
   end
+
+  def blog_status_color blog
+    'color: #d9534f;' if blog.draft?
+  end
 end
