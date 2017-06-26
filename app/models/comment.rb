ceeed4f67a11c2546555febcca98152de65b9592
Chazona <chznbaum@gmail.com>
2017-06-26 14:53:00 -0400

    Added created_at to blog form and listed comment count on blogs

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ApplicationRecord
   validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
 
   after_create_commit { CommentBroadcastJob.perform_later(self) }
+
+  def self.by_recent
+    order("created_at DESC")
+  end
 end
