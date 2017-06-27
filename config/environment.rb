98434094cfb11c3c14bf2e5b740bdf8eca242b96
Chazona <chznbaum@gmail.com>
2017-06-27 10:13:55 -0400

    Updated sendgrid domain for moving to custom domain

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
   :password => ENV["SENDGRID_API_KEY"],
-  :domain => 'herokuapp.com',
+  :domain => 'chazonabaum.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
